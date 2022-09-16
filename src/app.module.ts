9a1f73842531f92fa6608bc44c2fef0577e3dceb
JongeunKeum <jon9eun@gmail.com>
2022-09-16 19:26:47 +0900

    add chat module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardsModule } from './boards/boards.module';
 import { typeORMConfig } from './configs/typeorm.config';
 import { AuthModule } from './auth/auth.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeORMConfig),
     BoardsModule,
     AuthModule,
+    ChatModule,
   ],
 })
 export class AppModule {}
